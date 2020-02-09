064104207b7da2bf63ee3d480d94f4f379b4ce8a
dmitrysidorik <60389453+dmitrysidorik@users.noreply.github.com>
2020-02-09 20:06:54 +0300

    int

--- Sandbox/src/main/java/MyFirstProgram.java
+++ Sandbox/src/main/java/MyFirstProgram.java
@@ -2,6 +2,8 @@ public class MyFirstProgram {
 
   public static void main(String[] args) {
 System.out.println("Hello, world!");
+int l=5;
+System.out.println("Площадь квадрата со стороной " + l + " = " +(l*l));
 }
 
 }
