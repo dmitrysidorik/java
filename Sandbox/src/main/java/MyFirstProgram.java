75bda5fd76b3858c872b73bcf5bc55c55feb42a6
dmitrysidorik <60389453+dmitrysidorik@users.noreply.github.com>
2020-02-13 16:25:50 +0300

    Добавлено несколько функций

--- Sandbox/src/main/java/MyFirstProgram.java
+++ Sandbox/src/main/java/MyFirstProgram.java
@@ -1,9 +1,26 @@
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-System.out.println("Hello, world!");
-int l=5;
-System.out.println("Площадь квадрата со стороной " + l + " = " +(l*l));
-}
+    hello("world");
+    hello("user");
+    hello("Dmitry");
 
+    double l = 5;
+    System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+    double a = 4;
+    double b = 6;
+    System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a,b));
+  }
+
+  public static void hello(String somebody) {
+    System.out.println("Hello, " + somebody + "!");
+  }
+
+   public static double area(double len) {
+     return len * len;
+  }
+
+   public static double area(double a, double b) {
+     return a * b;
+  }
 }
